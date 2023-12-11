0663e0e17af266bbc76ffc8f28190ed9d234be99
annettenk <107181090+annettenk@users.noreply.github.com>
2023-12-11 17:49:33 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -2,7 +2,7 @@
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item">
-            <a href="a.php" class="nav-link active" aria-current="page">
+            <a href="a.php" class="nav-link" aria-current="page">
                 <div class="icon-wrap">
                     <i class="fas fa-star"></i>
                 </div>
@@ -44,3 +44,22 @@
     </ul>
     <hr>
 </div>
+
+<script>
+    // Get the current URL
+    var currentUrl = window.location.href;
+
+    // Get all the nav links
+    var navLinks = document.querySelectorAll('.nav-link');
+
+    // Loop through each nav link
+    navLinks.forEach(function(link) {
+    // Get the href attribute of the link
+    var linkUrl = link.getAttribute('href');
+    // Check if the current URL matches the link URL
+    if (currentUrl.indexOf(linkUrl) !== -1) {
+        // Add the active class to the link
+        link.classList.add('active');
+    }
+    });
+</script>
