c91cc880a9a882faa9ac781b10b608715d3d7546
annettenk <107181090+annettenk@users.noreply.github.com>
2023-12-11 16:48:08 +0700

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,46 @@
+<div class="d-flex flex-column flex-shrink-0 sidebar-wrap">
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+            <a href="a.php" class="nav-link active" aria-current="page">
+                <div class="icon-wrap">
+                    <i class="fas fa-star"></i>
+                </div>
+                <span>Rating</span>
+            </a>
+        </li>
+        <li>
+            <a href="b.php" class="nav-link">
+                <div class="icon-wrap">
+                    <i class="fas fa-thumbs-up"></i>
+                </div>
+                <span>Satisfaction</span>
+            </a>
+        </li>
+        <li>
+            <a href="c.php" class="nav-link">
+                <div class="icon-wrap">
+                    <i class="fas fa-users"></i>
+                </div>
+                <span>Training</span>
+            </a>
+        </li>
+        <li>
+            <a href="d.php" class="nav-link">
+                <div class="icon-wrap">
+                    <i class="fas fa-comment-dollar	"></i>
+                </div>
+                <span>Desired Salary</span>
+            </a>
+        </li>
+        <li>
+            <a href="e.php" class="nav-link">
+                <div class="icon-wrap">
+                    <i class="fas fa-hand-holding-usd"></i>
+                </div>
+                <span>Salary</span>
+            </a>
+        </li>
+    </ul>
+    <hr>
+</div>
